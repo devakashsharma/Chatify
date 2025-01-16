85441b24979b8532c74ba776a66d65200c19fe75
Akash Sharma <soulakash20002@gmail.com>
2025-01-16 13:56:15 +0530

    Image added

--- constants/images.ts
+++ constants/images.ts
@@ -7,8 +7,10 @@
 
 import login_white from "@/assets/images/login_white.png";
 import reactImage from "@/assets/images/partial-react-logo.png"
+import login_purple from "@/assets/images/login_purple.png"
 
 export default {
 	login_white,
-	reactImage
+	reactImage,
+	login_purple,
 }
